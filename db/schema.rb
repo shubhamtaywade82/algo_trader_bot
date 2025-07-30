26366bc7ee712fec479271adc1fde35b56afa192
Shubham Taywade <shubhamtaywade82@gmail.com>
2025-07-30 23:49:49 +0530

    Add Instrument model and migration for instruments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,55 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_30_175842) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "instruments", force: :cascade do |t|
+    t.string "exchange", null: false
+    t.string "segment", null: false
+    t.string "security_id", null: false
+    t.string "isin"
+    t.string "instrument_code"
+    t.integer "underlying_security_id"
+    t.string "underlying_symbol"
+    t.string "symbol_name"
+    t.string "display_name"
+    t.string "instrument_type"
+    t.string "series"
+    t.integer "lot_size"
+    t.date "expiry_date"
+    t.decimal "strike_price", precision: 15, scale: 5
+    t.string "option_type"
+    t.decimal "tick_size"
+    t.string "expiry_flag"
+    t.string "bracket_flag"
+    t.string "cover_flag"
+    t.string "asm_gsm_flag"
+    t.string "asm_gsm_category"
+    t.string "buy_sell_indicator"
+    t.decimal "buy_co_min_margin_per", precision: 8, scale: 2
+    t.decimal "sell_co_min_margin_per", precision: 8, scale: 2
+    t.decimal "buy_co_sl_range_max_perc", precision: 8, scale: 2
+    t.decimal "sell_co_sl_range_max_perc", precision: 8, scale: 2
+    t.decimal "buy_co_sl_range_min_perc", precision: 8, scale: 2
+    t.decimal "sell_co_sl_range_min_perc", precision: 8, scale: 2
+    t.decimal "buy_bo_min_margin_per", precision: 8, scale: 2
+    t.decimal "sell_bo_min_margin_per", precision: 8, scale: 2
+    t.decimal "buy_bo_sl_range_max_perc", precision: 8, scale: 2
+    t.decimal "sell_bo_sl_range_max_perc", precision: 8, scale: 2
+    t.decimal "buy_bo_sl_range_min_perc", precision: 8, scale: 2
+    t.decimal "sell_bo_sl_min_range", precision: 8, scale: 2
+    t.decimal "buy_bo_profit_range_max_perc", precision: 8, scale: 2
+    t.decimal "sell_bo_profit_range_max_perc", precision: 8, scale: 2
+    t.decimal "buy_bo_profit_range_min_perc", precision: 8, scale: 2
+    t.decimal "sell_bo_profit_range_min_perc", precision: 8, scale: 2
+    t.decimal "mtf_leverage", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["instrument_code"], name: "index_instruments_on_instrument_code"
+    t.index ["security_id", "symbol_name", "exchange", "segment"], name: "index_instruments_unique", unique: true
+    t.index ["symbol_name"], name: "index_instruments_on_symbol_name"
+    t.index ["underlying_symbol", "expiry_date"], name: "index_instruments_on_underlying_symbol_and_expiry_date", where: "(underlying_symbol IS NOT NULL)"
+  end
 end
